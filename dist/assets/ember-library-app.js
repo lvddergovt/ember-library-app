478a3012584903144104186ba5437fa0e2ad56b4
Guido Bakkes <gbakkes@gmail.com>
2017-01-04 22:19:25 +0100

    Set up project. Beginning of tutorial

---
+++ dist/assets/ember-library-app.js
@@ -0,0 +1,299 @@
+"use strict";
+
+/* jshint ignore:start */
+
+
+
+/* jshint ignore:end */
+
+define('ember-library-app/app', ['exports', 'ember', 'ember-library-app/resolver', 'ember-load-initializers', 'ember-library-app/config/environment'], function (exports, _ember, _emberLibraryAppResolver, _emberLoadInitializers, _emberLibraryAppConfigEnvironment) {
+
+  var App = undefined;
+
+  _ember['default'].MODEL_FACTORY_INJECTIONS = true;
+
+  App = _ember['default'].Application.extend({
+    modulePrefix: _emberLibraryAppConfigEnvironment['default'].modulePrefix,
+    podModulePrefix: _emberLibraryAppConfigEnvironment['default'].podModulePrefix,
+    Resolver: _emberLibraryAppResolver['default']
+  });
+
+  (0, _emberLoadInitializers['default'])(App, _emberLibraryAppConfigEnvironment['default'].modulePrefix);
+
+  exports['default'] = App;
+});
+define('ember-library-app/helpers/app-version', ['exports', 'ember', 'ember-library-app/config/environment'], function (exports, _ember, _emberLibraryAppConfigEnvironment) {
+  exports.appVersion = appVersion;
+  var version = _emberLibraryAppConfigEnvironment['default'].APP.version;
+
+  function appVersion() {
+    return version;
+  }
+
+  exports['default'] = _ember['default'].Helper.helper(appVersion);
+});
+define('ember-library-app/helpers/pluralize', ['exports', 'ember-inflector/lib/helpers/pluralize'], function (exports, _emberInflectorLibHelpersPluralize) {
+  exports['default'] = _emberInflectorLibHelpersPluralize['default'];
+});
+define('ember-library-app/helpers/singularize', ['exports', 'ember-inflector/lib/helpers/singularize'], function (exports, _emberInflectorLibHelpersSingularize) {
+  exports['default'] = _emberInflectorLibHelpersSingularize['default'];
+});
+define('ember-library-app/initializers/app-version', ['exports', 'ember-cli-app-version/initializer-factory', 'ember-library-app/config/environment'], function (exports, _emberCliAppVersionInitializerFactory, _emberLibraryAppConfigEnvironment) {
+  var _config$APP = _emberLibraryAppConfigEnvironment['default'].APP;
+  var name = _config$APP.name;
+  var version = _config$APP.version;
+  exports['default'] = {
+    name: 'App Version',
+    initialize: (0, _emberCliAppVersionInitializerFactory['default'])(name, version)
+  };
+});
+define('ember-library-app/initializers/container-debug-adapter', ['exports', 'ember-resolver/container-debug-adapter'], function (exports, _emberResolverContainerDebugAdapter) {
+  exports['default'] = {
+    name: 'container-debug-adapter',
+
+    initialize: function initialize() {
+      var app = arguments[1] || arguments[0];
+
+      app.register('container-debug-adapter:main', _emberResolverContainerDebugAdapter['default']);
+      app.inject('container-debug-adapter:main', 'namespace', 'application:main');
+    }
+  };
+});
+define('ember-library-app/initializers/data-adapter', ['exports', 'ember'], function (exports, _ember) {
+
+  /*
+    This initializer is here to keep backwards compatibility with code depending
+    on the `data-adapter` initializer (before Ember Data was an addon).
+  
+    Should be removed for Ember Data 3.x
+  */
+
+  exports['default'] = {
+    name: 'data-adapter',
+    before: 'store',
+    initialize: _ember['default'].K
+  };
+});
+define('ember-library-app/initializers/ember-data', ['exports', 'ember-data/setup-container', 'ember-data/-private/core'], function (exports, _emberDataSetupContainer, _emberDataPrivateCore) {
+
+  /*
+  
+    This code initializes Ember-Data onto an Ember application.
+  
+    If an Ember.js developer defines a subclass of DS.Store on their application,
+    as `App.StoreService` (or via a module system that resolves to `service:store`)
+    this code will automatically instantiate it and make it available on the
+    router.
+  
+    Additionally, after an application's controllers have been injected, they will
+    each have the store made available to them.
+  
+    For example, imagine an Ember.js application with the following classes:
+  
+    App.StoreService = DS.Store.extend({
+      adapter: 'custom'
+    });
+  
+    App.PostsController = Ember.Controller.extend({
+      // ...
+    });
+  
+    When the application is initialized, `App.ApplicationStore` will automatically be
+    instantiated, and the instance of `App.PostsController` will have its `store`
+    property set to that instance.
+  
+    Note that this code will only be run if the `ember-application` package is
+    loaded. If Ember Data is being used in an environment other than a
+    typical application (e.g., node.js where only `ember-runtime` is available),
+    this code will be ignored.
+  */
+
+  exports['default'] = {
+    name: 'ember-data',
+    initialize: _emberDataSetupContainer['default']
+  };
+});
+define('ember-library-app/initializers/export-application-global', ['exports', 'ember', 'ember-library-app/config/environment'], function (exports, _ember, _emberLibraryAppConfigEnvironment) {
+  exports.initialize = initialize;
+
+  function initialize() {
+    var application = arguments[1] || arguments[0];
+    if (_emberLibraryAppConfigEnvironment['default'].exportApplicationGlobal !== false) {
+      var theGlobal;
+      if (typeof window !== 'undefined') {
+        theGlobal = window;
+      } else if (typeof global !== 'undefined') {
+        theGlobal = global;
+      } else if (typeof self !== 'undefined') {
+        theGlobal = self;
+      } else {
+        // no reasonable global, just bail
+        return;
+      }
+
+      var value = _emberLibraryAppConfigEnvironment['default'].exportApplicationGlobal;
+      var globalName;
+
+      if (typeof value === 'string') {
+        globalName = value;
+      } else {
+        globalName = _ember['default'].String.classify(_emberLibraryAppConfigEnvironment['default'].modulePrefix);
+      }
+
+      if (!theGlobal[globalName]) {
+        theGlobal[globalName] = application;
+
+        application.reopen({
+          willDestroy: function willDestroy() {
+            this._super.apply(this, arguments);
+            delete theGlobal[globalName];
+          }
+        });
+      }
+    }
+  }
+
+  exports['default'] = {
+    name: 'export-application-global',
+
+    initialize: initialize
+  };
+});
+define('ember-library-app/initializers/injectStore', ['exports', 'ember'], function (exports, _ember) {
+
+  /*
+    This initializer is here to keep backwards compatibility with code depending
+    on the `injectStore` initializer (before Ember Data was an addon).
+  
+    Should be removed for Ember Data 3.x
+  */
+
+  exports['default'] = {
+    name: 'injectStore',
+    before: 'store',
+    initialize: _ember['default'].K
+  };
+});
+define('ember-library-app/initializers/store', ['exports', 'ember'], function (exports, _ember) {
+
+  /*
+    This initializer is here to keep backwards compatibility with code depending
+    on the `store` initializer (before Ember Data was an addon).
+  
+    Should be removed for Ember Data 3.x
+  */
+
+  exports['default'] = {
+    name: 'store',
+    after: 'ember-data',
+    initialize: _ember['default'].K
+  };
+});
+define('ember-library-app/initializers/transforms', ['exports', 'ember'], function (exports, _ember) {
+
+  /*
+    This initializer is here to keep backwards compatibility with code depending
+    on the `transforms` initializer (before Ember Data was an addon).
+  
+    Should be removed for Ember Data 3.x
+  */
+
+  exports['default'] = {
+    name: 'transforms',
+    before: 'store',
+    initialize: _ember['default'].K
+  };
+});
+define("ember-library-app/instance-initializers/ember-data", ["exports", "ember-data/-private/instance-initializers/initialize-store-service"], function (exports, _emberDataPrivateInstanceInitializersInitializeStoreService) {
+  exports["default"] = {
+    name: "ember-data",
+    initialize: _emberDataPrivateInstanceInitializersInitializeStoreService["default"]
+  };
+});
+define('ember-library-app/resolver', ['exports', 'ember-resolver'], function (exports, _emberResolver) {
+  exports['default'] = _emberResolver['default'];
+});
+define('ember-library-app/router', ['exports', 'ember', 'ember-library-app/config/environment'], function (exports, _ember, _emberLibraryAppConfigEnvironment) {
+
+  var Router = _ember['default'].Router.extend({
+    location: _emberLibraryAppConfigEnvironment['default'].locationType,
+    rootURL: _emberLibraryAppConfigEnvironment['default'].rootURL
+  });
+
+  Router.map(function () {
+    this.route('about');
+    this.route('contact');
+  });
+
+  exports['default'] = Router;
+});
+define('ember-library-app/routes/about', ['exports', 'ember'], function (exports, _ember) {
+  exports['default'] = _ember['default'].Route.extend({});
+});
+define('ember-library-app/routes/contact', ['exports', 'ember'], function (exports, _ember) {
+  exports['default'] = _ember['default'].Route.extend({});
+});
+define('ember-library-app/services/ajax', ['exports', 'ember-ajax/services/ajax'], function (exports, _emberAjaxServicesAjax) {
+  Object.defineProperty(exports, 'default', {
+    enumerable: true,
+    get: function get() {
+      return _emberAjaxServicesAjax['default'];
+    }
+  });
+});
+define("ember-library-app/templates/about", ["exports"], function (exports) {
+  exports["default"] = Ember.HTMLBars.template({ "id": "op4EQMTp", "block": "{\"statements\":[[\"open-element\",\"h1\",[]],[\"flush-element\"],[\"text\",\"About Page\"],[\"close-element\"],[\"text\",\"\\n\"]],\"locals\":[],\"named\":[],\"yields\":[],\"blocks\":[],\"hasPartials\":false}", "meta": { "moduleName": "ember-library-app/templates/about.hbs" } });
+});
+define("ember-library-app/templates/application", ["exports"], function (exports) {
+  exports["default"] = Ember.HTMLBars.template({ "id": "7qE3c9WV", "block": "{\"statements\":[[\"open-element\",\"div\",[]],[\"static-attr\",\"class\",\"container\"],[\"flush-element\"],[\"text\",\"\\n    \"],[\"partial\",\"navbar\"],[\"text\",\"\\n    \"],[\"append\",[\"unknown\",[\"outlet\"]],false],[\"text\",\"\\n\"],[\"close-element\"],[\"text\",\"\\n\"]],\"locals\":[],\"named\":[],\"yields\":[],\"blocks\":[],\"hasPartials\":true}", "meta": { "moduleName": "ember-library-app/templates/application.hbs" } });
+});
+define("ember-library-app/templates/contact", ["exports"], function (exports) {
+  exports["default"] = Ember.HTMLBars.template({ "id": "X7ydY+x1", "block": "{\"statements\":[[\"open-element\",\"h1\",[]],[\"flush-element\"],[\"text\",\"Contact\"],[\"close-element\"],[\"text\",\"\\n\"]],\"locals\":[],\"named\":[],\"yields\":[],\"blocks\":[],\"hasPartials\":false}", "meta": { "moduleName": "ember-library-app/templates/contact.hbs" } });
+});
+define("ember-library-app/templates/index", ["exports"], function (exports) {
+  exports["default"] = Ember.HTMLBars.template({ "id": "zOYyeohQ", "block": "{\"statements\":[[\"open-element\",\"h1\",[]],[\"flush-element\"],[\"text\",\"Home Page\"],[\"close-element\"],[\"text\",\"\\n\"]],\"locals\":[],\"named\":[],\"yields\":[],\"blocks\":[],\"hasPartials\":false}", "meta": { "moduleName": "ember-library-app/templates/index.hbs" } });
+});
+define("ember-library-app/templates/navbar", ["exports"], function (exports) {
+  exports["default"] = Ember.HTMLBars.template({ "id": "744Q/Xzj", "block": "{\"statements\":[[\"open-element\",\"nav\",[]],[\"static-attr\",\"class\",\"navbar navbar-inverse\"],[\"flush-element\"],[\"text\",\"\\n    \"],[\"open-element\",\"div\",[]],[\"static-attr\",\"class\",\"container-fluid\"],[\"flush-element\"],[\"text\",\"\\n        \"],[\"open-element\",\"div\",[]],[\"static-attr\",\"class\",\"navbar-header\"],[\"flush-element\"],[\"text\",\"\\n            \"],[\"open-element\",\"button\",[]],[\"static-attr\",\"type\",\"button\"],[\"static-attr\",\"class\",\"navbar-toggle collapsed\"],[\"static-attr\",\"data-toggle\",\"collapse\"],[\"static-attr\",\"data-target\",\"#main-navbar\"],[\"flush-element\"],[\"text\",\"\\n                \"],[\"open-element\",\"span\",[]],[\"static-attr\",\"class\",\"sr-only\"],[\"flush-element\"],[\"text\",\"Toggle navigation\"],[\"close-element\"],[\"text\",\"\\n                \"],[\"open-element\",\"span\",[]],[\"static-attr\",\"class\",\"icon-bar\"],[\"flush-element\"],[\"close-element\"],[\"text\",\"\\n                \"],[\"open-element\",\"span\",[]],[\"static-attr\",\"class\",\"icon-bar\"],[\"flush-element\"],[\"close-element\"],[\"text\",\"\\n                \"],[\"open-element\",\"span\",[]],[\"static-attr\",\"class\",\"icon-bar\"],[\"flush-element\"],[\"close-element\"],[\"text\",\"\\n            \"],[\"close-element\"],[\"text\",\"\\n            \"],[\"block\",[\"link-to\"],[\"index\"],[[\"class\"],[\"navbar-brand\"]],3],[\"text\",\"\\n        \"],[\"close-element\"],[\"text\",\"\\n\\n        \"],[\"open-element\",\"div\",[]],[\"static-attr\",\"class\",\"collapse navbar-collapse\"],[\"static-attr\",\"id\",\"main-navbar\"],[\"flush-element\"],[\"text\",\"\\n            \"],[\"open-element\",\"ul\",[]],[\"static-attr\",\"class\",\"nav navbar-nav\"],[\"flush-element\"],[\"text\",\"\\n                \"],[\"block\",[\"link-to\"],[\"index\"],[[\"tagName\"],[\"li\"]],2],[\"text\",\"\\n                \"],[\"block\",[\"link-to\"],[\"about\"],[[\"tagName\"],[\"li\"]],1],[\"text\",\"\\n                \"],[\"block\",[\"link-to\"],[\"contact\"],[[\"tagName\"],[\"li\"]],0],[\"text\",\"\\n            \"],[\"close-element\"],[\"text\",\"\\n        \"],[\"close-element\"],[\"text\",\"\\n    \"],[\"close-element\"],[\"text\",\"\\n\"],[\"close-element\"],[\"text\",\"\\n\"]],\"locals\":[],\"named\":[],\"yields\":[],\"blocks\":[{\"statements\":[[\"open-element\",\"a\",[]],[\"static-attr\",\"href\",\"\"],[\"flush-element\"],[\"text\",\"Contact\"],[\"close-element\"]],\"locals\":[]},{\"statements\":[[\"open-element\",\"a\",[]],[\"static-attr\",\"href\",\"\"],[\"flush-element\"],[\"text\",\"About\"],[\"close-element\"]],\"locals\":[]},{\"statements\":[[\"open-element\",\"a\",[]],[\"static-attr\",\"href\",\"\"],[\"flush-element\"],[\"text\",\"Home\"],[\"close-element\"]],\"locals\":[]},{\"statements\":[[\"text\",\" Library App \"]],\"locals\":[]}],\"hasPartials\":false}", "meta": { "moduleName": "ember-library-app/templates/navbar.hbs" } });
+});
+/* jshint ignore:start */
+
+
+
+/* jshint ignore:end */
+
+/* jshint ignore:start */
+
+define('ember-library-app/config/environment', ['ember'], function(Ember) {
+  var prefix = 'ember-library-app';
+/* jshint ignore:start */
+
+try {
+  var metaName = prefix + '/config/environment';
+  var rawConfig = document.querySelector('meta[name="' + metaName + '"]').getAttribute('content');
+  var config = JSON.parse(unescape(rawConfig));
+
+  var exports = { 'default': config };
+
+  Object.defineProperty(exports, '__esModule', { value: true });
+
+  return exports;
+}
+catch(err) {
+  throw new Error('Could not read config from meta tag with name "' + metaName + '".');
+}
+
+/* jshint ignore:end */
+
+});
+
+/* jshint ignore:end */
+
+/* jshint ignore:start */
+
+if (!runningTests) {
+  require("ember-library-app/app")["default"].create({"LOG_ACTIVE_GENERATION":true,"LOG_TRANSITIONS":true,"LOG_TRANSITIONS_INTERNAL":true,"LOG_VIEW_LOOKUPS":true,"name":"ember-library-app","version":"0.0.0+"});
+}
+
+/* jshint ignore:end */
+//# sourceMappingURL=ember-library-app.map
---
+++ dist/assets/tests.js
@@ -0,0 +1,207 @@
+'use strict';
+
+define('ember-library-app/tests/app.jshint.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint | app.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'app.js should pass jshint.');
+  });
+});
+define('ember-library-app/tests/helpers/destroy-app', ['exports', 'ember'], function (exports, _ember) {
+  exports['default'] = destroyApp;
+
+  function destroyApp(application) {
+    _ember['default'].run(application, 'destroy');
+  }
+});
+define('ember-library-app/tests/helpers/destroy-app.jshint.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint | helpers/destroy-app.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/destroy-app.js should pass jshint.');
+  });
+});
+define('ember-library-app/tests/helpers/module-for-acceptance', ['exports', 'qunit', 'ember', 'ember-library-app/tests/helpers/start-app', 'ember-library-app/tests/helpers/destroy-app'], function (exports, _qunit, _ember, _emberLibraryAppTestsHelpersStartApp, _emberLibraryAppTestsHelpersDestroyApp) {
+  var Promise = _ember['default'].RSVP.Promise;
+
+  exports['default'] = function (name) {
+    var options = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
+
+    (0, _qunit.module)(name, {
+      beforeEach: function beforeEach() {
+        this.application = (0, _emberLibraryAppTestsHelpersStartApp['default'])();
+
+        if (options.beforeEach) {
+          return options.beforeEach.apply(this, arguments);
+        }
+      },
+
+      afterEach: function afterEach() {
+        var _this = this;
+
+        var afterEach = options.afterEach && options.afterEach.apply(this, arguments);
+        return Promise.resolve(afterEach).then(function () {
+          return (0, _emberLibraryAppTestsHelpersDestroyApp['default'])(_this.application);
+        });
+      }
+    });
+  };
+});
+define('ember-library-app/tests/helpers/module-for-acceptance.jshint.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint | helpers/module-for-acceptance.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/module-for-acceptance.js should pass jshint.');
+  });
+});
+define('ember-library-app/tests/helpers/resolver', ['exports', 'ember-library-app/resolver', 'ember-library-app/config/environment'], function (exports, _emberLibraryAppResolver, _emberLibraryAppConfigEnvironment) {
+
+  var resolver = _emberLibraryAppResolver['default'].create();
+
+  resolver.namespace = {
+    modulePrefix: _emberLibraryAppConfigEnvironment['default'].modulePrefix,
+    podModulePrefix: _emberLibraryAppConfigEnvironment['default'].podModulePrefix
+  };
+
+  exports['default'] = resolver;
+});
+define('ember-library-app/tests/helpers/resolver.jshint.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint | helpers/resolver.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/resolver.js should pass jshint.');
+  });
+});
+define('ember-library-app/tests/helpers/start-app', ['exports', 'ember', 'ember-library-app/app', 'ember-library-app/config/environment'], function (exports, _ember, _emberLibraryAppApp, _emberLibraryAppConfigEnvironment) {
+  exports['default'] = startApp;
+
+  function startApp(attrs) {
+    var application = undefined;
+
+    // use defaults, but you can override
+    var attributes = _ember['default'].assign({}, _emberLibraryAppConfigEnvironment['default'].APP, attrs);
+
+    _ember['default'].run(function () {
+      application = _emberLibraryAppApp['default'].create(attributes);
+      application.setupForTesting();
+      application.injectTestHelpers();
+    });
+
+    return application;
+  }
+});
+define('ember-library-app/tests/helpers/start-app.jshint.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint | helpers/start-app.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/start-app.js should pass jshint.');
+  });
+});
+define('ember-library-app/tests/resolver.jshint.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint | resolver.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'resolver.js should pass jshint.');
+  });
+});
+define('ember-library-app/tests/router.jshint.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint | router.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'router.js should pass jshint.');
+  });
+});
+define('ember-library-app/tests/routes/about.jshint.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint | routes/about.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'routes/about.js should pass jshint.');
+  });
+});
+define('ember-library-app/tests/routes/contact.jshint.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint | routes/contact.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'routes/contact.js should pass jshint.');
+  });
+});
+define('ember-library-app/tests/test-helper', ['exports', 'ember-library-app/tests/helpers/resolver', 'ember-qunit'], function (exports, _emberLibraryAppTestsHelpersResolver, _emberQunit) {
+
+  (0, _emberQunit.setResolver)(_emberLibraryAppTestsHelpersResolver['default']);
+});
+define('ember-library-app/tests/test-helper.jshint.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint | test-helper.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'test-helper.js should pass jshint.');
+  });
+});
+define('ember-library-app/tests/unit/routes/about-test', ['exports', 'ember-qunit'], function (exports, _emberQunit) {
+
+  (0, _emberQunit.moduleFor)('route:about', 'Unit | Route | about', {
+    // Specify the other units that are required for this test.
+    // needs: ['controller:foo']
+  });
+
+  (0, _emberQunit.test)('it exists', function (assert) {
+    var route = this.subject();
+    assert.ok(route);
+  });
+});
+define('ember-library-app/tests/unit/routes/about-test.jshint.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint | unit/routes/about-test.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/routes/about-test.js should pass jshint.');
+  });
+});
+define('ember-library-app/tests/unit/routes/contact-test', ['exports', 'ember-qunit'], function (exports, _emberQunit) {
+
+  (0, _emberQunit.moduleFor)('route:contact', 'Unit | Route | contact', {
+    // Specify the other units that are required for this test.
+    // needs: ['controller:foo']
+  });
+
+  (0, _emberQunit.test)('it exists', function (assert) {
+    var route = this.subject();
+    assert.ok(route);
+  });
+});
+define('ember-library-app/tests/unit/routes/contact-test.jshint.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint | unit/routes/contact-test.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/routes/contact-test.js should pass jshint.');
+  });
+});
+/* jshint ignore:start */
+
+require('ember-library-app/tests/test-helper');
+EmberENV.TESTS_FILE_LOADED = true;
+
+/* jshint ignore:end */
+//# sourceMappingURL=tests.map
